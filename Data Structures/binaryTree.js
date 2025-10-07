073969db16392efe727a65b048ff82b0745a2544
Bryan Guillén <bryanguillenlds@gmail.com>
2025-10-07 09:29:37 -0400

    Implemented BST remove method.

--- Data Structures/binaryTree.js
+++ Data Structures/binaryTree.js
@@ -72,80 +72,109 @@ class BinarySearchTree {
     let currentNodeParent = null;
     let cameFrom = null;
 
-    while(currentNode) {
+    // Loop while there is a node
+    while (currentNode) {
+      // If we found the node
       if (value === currentNode.value) {
-        //figure out re-linking
-
-        //No children / leaf node
+        // 1. NODE HAS NO CHILDREN (leaf node)
         if (!currentNode.right && !currentNode.left) {
+          // if the node is a lone root with no children
+          // set it to null and return it
           if (!currentNodeParent) {
-            // Removing lone root with no children
             this.root = null;
           } else if (cameFrom === 'R') {
+            //if we came to the right, make parent point to null
+            //to unlink the child
             currentNodeParent.right = null;
           } else {
+            //if we came to the left, make parent point to null
+            //to unlink the child
             currentNodeParent.left = null;
           }
         }
 
-        //One child (right)
+        // 2. NODE HAS ONLY 1 CHILD (right)
         if (currentNode.right && !currentNode.left) {
           if (!currentNodeParent) {
-            // Removing lone root with one child
+            // Removing lone root with one child and assigning new root
             this.root = currentNode.right;
           } else if (cameFrom === 'L') {
+            //if we came to the left, make parent point to the left to it's right grandchild
+            //to unlink the child
             currentNodeParent.left = currentNode.right;
           } else {
+            //if we came to the right, make parent point to the right to it's right grandchild
+            //to unlink the child
             currentNodeParent.right = currentNode.right;
           }
         } else if (currentNode.left && !currentNode.right) {
-          //One child (left)
+          //ONE CHILD (LEFT CHILD ONLY)
           if (!currentNodeParent) {
-            // Removing lone root with one child
+            // Removing lone root with one child and assigning new root
             this.root = currentNode.left;
           } else if (cameFrom === 'L') {
+            //if we came to the left, make parent point to the left to it's left grandchild
+            //to unlink the child
             currentNodeParent.left = currentNode.left;
           } else {
+            //if we came to the right, make parent point to the right to it's left grandchild
+            //to unlink the child
             currentNodeParent.right = currentNode.left;
           }
         }
 
-        // Two children:
-        // go to right child, then keep going left until left is null (leftmost).
-        // Copy its value here, then delete that successor node from its original spot (it has no left child).
+        // 3. NODE HAS TWO CHILDREN
+        // My Policy (pick smallest from bigger child):
+        // Go to right child once, then keep going left until left is null (leftmost leaf node).
+        // Copy its value into the node to be removed,
+        // then delete that successor node from its original spot (it has no left child).
         if (currentNode.right && currentNode.left) {
           let successorParent = currentNode;
           let successorNode = currentNode.right;
 
-          // Find the leftmost node in the right subtree (stop when left is null)
+          // Find the leftmost node in the right subtree (stop when left is null
+          // meaning we reached the leftmost node. It may have a right child, but no left anymore)
           while(successorNode.left) {
             successorParent = successorNode;
             successorNode = successorNode.left;
           }
 
           // Copy the successor node's value to the current node
+          // we will unlink the successor node later but we want it's value to replace
+          // the value of the node to be deleted
           currentNode.value = successorNode.value;
 
-          // if the successor was the immediate right child of the current node
+          // if the successor was the immediate right child of the node to be removed
           if (successorParent === currentNode) {
-            // we know the successor right pointer is null, so we can just point to the right child of the successor
+            // We can just point to the right child of the successor
+            // (Successor has no left child by definition; its right may exist or be null, so we point to that)
             successorParent.right = successorNode.right;
           } else {
             // successor was somewhere down the left chain of the right subtree
+            // now we make it point to the right child of the successor
+            // we are just disconnecting the successor
+            // (because we already got its value copied to the node to be removed)
+            // so garbage collection will take care of it
             successorParent.left = successorNode.right;
           }
         }
+
+        return true;
       }
 
-      if (value < currentNode.value) {
-        currentNodeParent = currentNode;
-        currentNode = currentNode.left;
-        cameFrom = 'L';
+      // if current is less than value we are looking for
+      if (currentNode.value < value) {
+        currentNodeParent = currentNode; //make currentnode new parent for next iteration
+        currentNode = currentNode.right; //advance to where the bigger nodes are
+        cameFrom = 'R'; //signal that we have moved to the right
       } else {
-        currentNodeParent = currentNode;
-        currentNode = currentNode.right;
-        cameFrom = 'R';
+        // if current is greater than value we're looking for
+        currentNodeParent = currentNode; //make currentnode new parent for next iteration
+        currentNode = currentNode.left; //advance to where the smaller nodes are
+        cameFrom = 'L'; //signal that we have moved to the left
       }
     }
+
+    return false;
   }
 }
